9d415269f69d7e0360629da558147e0a683905c8
Kaiose31 <saurabhpahwa31@gmail.com>
2020-09-15 04:27:23 +0530

    model output works on any given image with CUI

---
+++ formatter.py
@@ -0,0 +1,25 @@
+import tensorflow as tf 
+from tensorflow import keras  
+import cv2
+import numpy as np
+
+model = keras.models.load_model('num_model.h5')
+
+#formatting the input image to input to the model 
+def classifier(path):
+    test = cv2.imread(path,0)
+    test=cv2.bitwise_not(test)
+    test = cv2.resize(test,(28,28))
+    test = test/255.0
+    test = np.array(test)
+    test = np.expand_dims(test,axis=0)
+    
+    prediction = model.predict(test) #Output from the model on the single image
+    
+    print(np.argmax(prediction)) 
+
+
+
+
+path = input("enter image path: ")
+classifier(path)    #input is path of the image. prints the predicted digit
